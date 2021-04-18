7361be2437e475ff925b780b34e4be558895effb
fre-ben <fschocke@gmail.com>
2021-04-18 16:10:00 +0200

    Pass data to a parent component https://angular.io/start#pass-data-to-a-parent-component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
 export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert("The product has been shared!");
+  }
+
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale");
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
